322a218ea305d05dc5ccf405851df2ff301c624c
alexey-anokhin <alexey-anokhin@mail.ru>
2021-04-09 14:41:12 +0300

    Добавить страницу описание книги

---
+++ presentanion/Store.Web/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookRepository bookRepository;
+        private int id;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            this.bookRepository = bookRepository;
+        }
+
+        public IActionResult Index()
+        {
+            Book book = bookRepository.GetById(id);
+            return View(book);
+        }
+    }
+}
