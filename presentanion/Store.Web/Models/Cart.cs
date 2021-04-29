7637d0d57134e1f5d91dcf6a8f9631a98ed4cc2f
alexey-anokhin <alexey-anokhin@mail.ru>
2021-04-29 07:28:51 +0300

    Добавить заказ и хранилище заказов

--- presentanion/Store.Web/Models/Cart.cs
+++ presentanion/Store.Web/Models/Cart.cs
@@ -4,9 +4,23 @@ namespace Store.Web.Models
 {
     public class Cart
     {
-        public Dictionary<int, int> Items { get; set; } = new Dictionary<int, int>();
+        internal object Items;
 
-        public decimal Amount { get; set; }
+        public int OrderId { get; }
 
+        public int TotalCount { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        public Cart(int orderId)
+        {
+            OrderId = orderId;
+            TotalCount = 0;
+            TotalPrice = 0m;
+        }
+
+        public Cart()
+        {
+        }
     }
 }
